c6695e7cac752960e4a6272393123e5353c1865e
Henry Barcalow <henry.barcalow@gmail.com>
2026-04-08 13:55:10 -0400

    cartoonishly bad fireball

--- Assets/Scripts/PlayerAttack/MagicMissile.cs
+++ Assets/Scripts/PlayerAttack/MagicMissile.cs
@@ -1,75 +1,51 @@
 using Interfaces;
-using System.Collections;
 using UnityEngine;
 
-public class MagicMissile : MonoBehaviour
+public class MagicMissile : Projectile
 {
     private GameObject target;
+    
+    // THIS MUST BE SET IN PREFAB--the normal speed setter doesn't work.
     public float speed = 10f;
-    public float rotationSpeed = 200f;
-    public float damage = 20f;
     [SerializeField] private float lifetime = 5f;
-
-    private Rigidbody rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 100f))
-        {
-            if (hit.collider.CompareTag("Enemy"))
-            {
-                target = hit.collider.gameObject;
-            }
-        }
-        if (target == null)
-        {
-            target = FindClosestEnemy();
-        }
-
+        FindTarget();
 
         Destroy(gameObject, lifetime);
     }
 
     void FixedUpdate()
     {
-        if (target != null)
+        if (target)
         {
             Vector3 direction = (target.transform.position - transform.position).normalized;
             rb.AddForce(direction * speed);
         }
-        else {
+        else
+        {
             rb.AddForce(transform.forward * speed);
+            target = FindTarget();
         }
     }
 
-    public void SetTarget(GameObject newTarget)
+    GameObject FindTarget()
     {
-        target = newTarget;
-    }
-
-    GameObject FindClosestEnemy()
-    {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        GameObject closestEnemy = null;
-        float minDistance = Mathf.Infinity;
-
-        foreach (GameObject enemy in enemies)
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit, 100f))
         {
-            float distance = Vector3.Distance(transform.position, enemy.transform.position);
-            if (distance < minDistance)
+            if (hit.collider)
             {
-                minDistance = distance;
-                closestEnemy = enemy;
+                return hit.collider.gameObject;
             }
         }
-        if (closestEnemy == null) return null;
 
-        return closestEnemy;
+        return null;
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
--- Assets/Scripts/PlayerAttack/Projectile.cs
+++ Assets/Scripts/PlayerAttack/Projectile.cs
@@ -7,38 +7,33 @@ public class Projectile : MonoBehaviour
     public float damage;
     public Rigidbody rb;
 
+    [SerializeField] float lifetime = 5f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         if (rb == null) Debug.LogError($"No Rigidbody found on {name}");
+        
+        if (lifetime > 0) Destroy(gameObject, lifetime);
     }
 
     public void OnCollisionEnter(Collision collision)
     {
         ContactPoint contact = collision.contacts[0];
         // Prevent projectiles from colliding with each other.
-        if (contact.otherCollider.GetType() == contact.thisCollider.GetType()) return;
+        if (contact.otherCollider.GetType() == contact.thisCollider.GetType()
+            || contact.otherCollider.gameObject.CompareTag("Player")) return;
         
         IDamageable damageable = contact.otherCollider.GetComponent<IDamageable>();
 
         print($"Doing {damage} to {contact.otherCollider.name}");
 
-        if (damageable is { IsPlayer: false })
-        {
-            damageable.TakeDamage(damage);
-        }
+        damageable.TakeDamage(damage);
         Destroy(gameObject);
     }
 
     public void Fire(float speed)
     {
         rb.AddForce(transform.forward * speed, ForceMode.Impulse);
-        StartCoroutine(Lifetime());
-    }
-
-    IEnumerator Lifetime()
-    {
-        yield return new WaitForSeconds(5f);
-        Destroy(gameObject);
     }
 }
