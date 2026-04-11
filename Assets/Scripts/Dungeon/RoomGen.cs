a9b916795e87e75032592a01a745c61b7575bf5d
Henry Barcalow <henry.barcalow@gmail.com>
2026-04-11 06:19:11 -0400

    well crap it kinda works

--- Assets/Scripts/Dungeon/RoomGen.cs
+++ Assets/Scripts/Dungeon/RoomGen.cs
@@ -11,8 +11,8 @@ public class RoomGen : MonoBehaviour
     public int maxRooms = 15;
 
     [Header("Table Bounds (centered on this position)")]
-    public Vector2 tableSize = new Vector2(2, 6);
-    public Vector3 tableCenter = new Vector3(0, 0, 1);
+    public Vector2 tableSize;
+    public Vector3 tableCenter;
 
     private List<Room> spawnedRooms = new List<Room>();
     private List<Bounds> spawnedBounds = new List<Bounds>();
@@ -33,7 +33,7 @@ public class RoomGen : MonoBehaviour
         spawnedRooms.Clear();
         spawnedBounds.Clear();
 
-        Room startRoom = Instantiate(startRoomPrefab, new Vector3(0, transform.position.y, 0), Quaternion.identity);
+        Room startRoom = Instantiate(startRoomPrefab, transform.position, Quaternion.identity);
         RegisterRoom(startRoom);
 
         List<ConnectorTransform> openConnectors = new List<ConnectorTransform>(startRoom.connectors);
