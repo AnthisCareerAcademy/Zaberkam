80596711a917b670430cfe968e05c1bc48b4e687
Henry Barcalow <henry.barcalow@gmail.com>
2026-04-27 12:25:27 -0400

    Create dummy spawners

--- Assets/Scripts/VR/PlacementProperties.cs
+++ Assets/Scripts/VR/PlacementProperties.cs
@@ -1,13 +1,22 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Interfaces
 {
-    public class PlacementProperties
+    public class PlacementProperties : MonoBehaviour
     {
         // Place this on ALL object spawners.
         public GameObject SpawnedItem;
-        public Mesh Mesh;
-        public Material[] Materials;
+        [SerializeField] GameObject visual;
+        [NonSerialized] public Mesh Mesh;
+        [NonSerialized] public Material[] Materials;
         public float Cost;
+
+        public void Start()
+        {
+            Mesh = visual.GetComponent<MeshFilter>().sharedMesh;
+            Materials = visual.GetComponent<MeshRenderer>().sharedMaterials;
+        }
     }
 }
